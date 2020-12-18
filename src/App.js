b53da5ad10363dd4ccfbcbc1308c869d97e26e31
ibnHamdan <ibnHamdan@msn.com>
2020-12-19 00:17:19 +0300

    Added react router v5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./App.css";
 import Layout from "./features/layout";
+import Home from "./features/home/Home";
 import Login from "./features/login/Login";
+import Regist from "./features/registration/regist";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Layout>
-      <Login />
-      <br />
-    </Layout>
+    <Router>
+      <Layout>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/regist">
+          <Regist />
+        </Route>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ src/features/registration/regist.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function Regist() {
+  return (
+    <>
+      <h1>Regist</h1>
+    </>
+  );
+}
+
+export default Regist;
